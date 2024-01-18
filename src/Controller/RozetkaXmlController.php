8f95ad3af77ae3ed61d860899219704ffbf3ddce
Andy <micktt7@gmail.com>
2024-01-18 16:27:54 +0200

    Create RozetkaXmlController.php

---
+++ src/Controller/RozetkaXmlController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\rozetka_xml_generator\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class RozetkaXmlController extends ControllerBase {
+  // Functions to generate XML.
+}
